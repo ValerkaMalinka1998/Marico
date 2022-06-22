fe9d5241e1a8ef7727d34fb4b84d1acd53f6a794
ValerkaMalinka1998 <46463165+ValerkaMalinka1998@users.noreply.github.com>
2022-06-22 03:20:14 +0300

    Ad

---
+++ js/jsScript.js
@@ -0,0 +1,14 @@
+// let page = document.querySelector('.container');
+// let butClck = document.querySelector('.btn');
+
+// butClck.onclick = function() {
+//   page.classList.add();
+//   page.classList.remove();
+//   page.classList.toggle();
+// }
+
+// formCl.onsubmit = function(evt) {
+//   page.textContent = '';
+//   mers.textContent = input.value;
+//   console.log(page);
+// }
