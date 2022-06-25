8d7382e4f68e3c00bf7252044d43b18850485260
ValerkaMalinka1998 <46463165+ValerkaMalinka1998@users.noreply.github.com>
2022-06-26 02:54:36 +0300

    The End

--- js/jsScript.js
+++ js/jsScript.js
@@ -12,3 +12,21 @@
 //   mers.textContent = input.value;
 //   console.log(page);
 // }
+
+// let page = document.querySelector('.container');
+// let btn = document.querySelector('.btn');
+
+// let form = document.querySelector('.form');
+
+// btn.onclick = function() {
+//   page.classList.add();
+//   page.classList.remove();
+//   page.classList.toggle();
+
+// }
+
+// form.onsubmit = function() {
+//   page.textContent = 'dsfsdf';
+//   page.textContent = input.value;
+//   console.log(page);
+// }
